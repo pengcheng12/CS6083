28ff2b7f90bfa2283a39f5830f10ad50d8c53abc
peng cheng <pengcheng@10-18-1-80.dynapool.wireless.nyu.edu>
2019-12-05 13:10:12 -0500

    start project

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>
+            welcome to nextdoor
+        </title>
+    </head>
+
+    <body>
+        <div id = "inputContainer">
+            
+        
+        </div>
+    </body>
+
+
+</html>
