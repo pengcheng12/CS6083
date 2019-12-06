7cebab4da27aaec40e63d07940ab7623940002c6
peng cheng <pengcheng@pengs-MBP-2.home>
2019-12-06 16:50:56 -0500

    render the login page

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php		
+	include("includes/config.php");
+?>
+	
+
+<body>
+    <div id = "inputContainer"> 
+        <form id = "loginForm" method = "POST">
+		<h2> Login to your account </h2>
+			<input id = "username" name = "username" type="text" placeholder = "gmail">
+			 
+		</form>
+
+    </div>
+</body>
